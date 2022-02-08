b8a457f1c83d8ff8ff39055e61c4a3eb4b09c1b9
Cédric Elslander <cedric.elslander@gmail.com>
2022-02-08 14:28:59 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,1 +1,18 @@
+/ WIFI
+#define SECRET_SSID "ssid"
+#define SECRET_PASS "password"
+
+// SHORTCUT
+#define SECRET_TOKEN "token"
+
+// BUZZER
 #define BUZZER_PIN 8
+
+// LCD
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
+
+// SERVO
+#define SERVO_PIN 3
+#define SERVO_SPEED 30
+#define SERVO_DELAY 1000
